8e92c63141c75006c85053e5d666a91fb00ed2d9
DheerajKN <kndheeraj0@gmail.com>
2021-10-14 14:08:17 +0100

    Initial Commit

---
+++ backend/server.ts
@@ -0,0 +1,17 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import "https://deno.land/x/dotenv/load.ts";
+
+const app = new Application();
+const port: number = 9001;
+
+const router = new Router();
+router.get("/", ({ response }: { response: any }) => {
+  response.body = {
+    message: "Backend Running",
+  };
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log('running on port ', Deno.env.get("BACKEND_PORT"));
+await app.listen({ port });
